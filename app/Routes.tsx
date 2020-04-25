e2f51af437db257c4a1f5323cb783c27cdb1c862
Mason Fish <mason.fish@docker.com>
2020-04-25 14:00:27 -0700

    fix default route
    Signed-off-by: Mason Fish <mason.fish@docker.com>

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -6,6 +6,7 @@ import JournalPage from './containers/JournalPage';
 import MainPage from './containers/MainPage';
 import TopNav from './components/TopNav';
 import AppContentWrapper from './components/AppContentWrapper';
+import { Redirect } from 'react-router';
 
 export default function Routes() {
   return (
@@ -13,8 +14,9 @@ export default function Routes() {
       <TopNav />
       <AppContentWrapper>
         <Switch>
-          <Route path={routes.JOURNAL} component={JournalPage} />
+          <Redirect exact from="/" to={routes.MAIN} />
           <Route path={routes.MAIN} component={MainPage} />
+          <Route path={routes.JOURNAL} component={JournalPage} />
           <Route path={routes.NEXT} component={() => <div>Next Project</div>} />
         </Switch>
       </AppContentWrapper>
