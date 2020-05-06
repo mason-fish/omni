f5dab98923d24973fa561a0f60dcb13630720217
Mason Fish <mason.fish@docker.com>
2020-05-05 17:03:25 -0700

    rename
    Signed-off-by: Mason Fish <mason.fish@docker.com>

--- app/components/OmniLogo/index.ts
+++ app/components/OmniLogo/index.ts
@@ -1,6 +1,6 @@
 // import React from 'react';
 
-// import Omni from "./omni.svg"
+// import Omni from './omni.svg';
 //
 // export default function OmniLogo(props: any) {
 //   return <Omni {...props} />
